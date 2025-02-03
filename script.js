78f19586a3b8857800999a21c99cc8e63a093a71
nav-mohan <navaneeth.mohan94@gmail.com>
2025-02-02 21:25:28 -0500

    initialize circle

--- script.js
+++ script.js
@@ -7,5 +7,16 @@ window.addEventListener('load',(e)=> {
     __G_CIRCLE["RADIUS"]            = __G_CANVAS["CANVAS"].width / 2;
     __G_CIRCLE["CENTER_X"]          = __G_CANVAS["CANVAS_WIDTH"] / 2;
     __G_CIRCLE["CENTER_Y"]          = __G_CANVAS["CANVAS_HEIGHT"] / 2;
+
+    initializeCircle();
 })
+
+// Draw the circle boundary
+function initializeCircle()
+{
+    __G_CANVAS["CANVAS_CONTEXT"].beginPath();
+    __G_CANVAS["CANVAS_CONTEXT"].arc(__G_CIRCLE["CENTER_X"], __G_CIRCLE["CENTER_Y"], __G_CIRCLE["RADIUS"], 0, 2 * Math.PI);
+    __G_CANVAS["CANVAS_CONTEXT"].strokeStyle = "white";
+    __G_CANVAS["CANVAS_CONTEXT"].stroke();
+}
 
