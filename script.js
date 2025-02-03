15158156a1ecf36a4e199355785ed4b596bfb276
nav-mohan <navaneeth.mohan94@gmail.com>
2025-02-02 22:23:05 -0500

    utility function for stepwise animation

--- script.js
+++ script.js
@@ -53,3 +53,37 @@ function drawPoint(x, y, color)
     __G_CANVAS["CANVAS_CONTEXT"].fill();
 }
 
+function animationStep() 
+{
+    if(__G_ANIMATION["TOTAL_POINTS"] >= __G_ANIMATION["NUM_POINTS"]) return;
+
+    for (let i = 0; i < __G_ANIMATION["BATCH_SIZE"] && __G_ANIMATION["TOTAL_POINTS"] < __G_ANIMATION["NUM_POINTS"]; i++) 
+    {
+        // Random point coordinates
+        const x = Math.random() * __G_CANVAS["CANVAS_WIDTH"];
+        const y = Math.random() * __G_CANVAS["CANVAS_HEIGHT"];
+
+        const distance = Math.sqrt(Math.pow(x - __G_CIRCLE["CENTER_X"], 2) + Math.pow(y - __G_CIRCLE["CENTER_Y"], 2));
+
+        // Color point based on position
+        if (distance <= __G_CIRCLE["RADIUS"]) 
+        {
+            __G_ANIMATION["INSIDE_CIRCLE"]++;
+            drawPoint(x, y, '#ff9800');
+        } 
+        else 
+        {
+            drawPoint(x, y, '#0f9bec');
+        }
+
+        __G_ANIMATION["TOTAL_POINTS"]++;
+    }
+
+    const piEstimate = 4 * (__G_ANIMATION["INSIDE_CIRCLE"] / __G_ANIMATION["TOTAL_POINTS"]);
+    const progressValue = (__G_ANIMATION["TOTAL_POINTS"] / __G_ANIMATION["NUM_POINTS"]) * 100;
+    updateProgress(progressValue);
+    updateStats(piEstimate);
+
+    requestAnimationFrame(animationStep);
+}
+
